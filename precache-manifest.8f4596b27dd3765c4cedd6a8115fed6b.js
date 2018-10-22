ea02752f25369540a9283c460ce309a289694805
unknown <auchiyam1994@gmail.com>
2018-10-22 12:26:23 -0500

    Updates

---
+++ precache-manifest.8f4596b27dd3765c4cedd6a8115fed6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3fe1b326a6a9d37c172",
+    "url": "/StudySeatHost/static/js/runtime~main.a3fe1b32.js"
+  },
+  {
+    "revision": "4743515cc4cd675e256e",
+    "url": "/StudySeatHost/static/js/main.4743515c.chunk.js"
+  },
+  {
+    "revision": "c2c164d2bd3ec92c1468",
+    "url": "/StudySeatHost/static/js/1.c2c164d2.chunk.js"
+  },
+  {
+    "revision": "4743515cc4cd675e256e",
+    "url": "/StudySeatHost/static/css/main.51f47dfc.chunk.css"
+  },
+  {
+    "revision": "813bbe69a016a1cc384d7ec7a3fc5f7a",
+    "url": "/StudySeatHost/index.html"
+  }
+];
