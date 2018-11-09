81bd98dbed0a9550a353745b09aa826871e5d5d4
unknown <auchiyam1994@gmail.com>
2018-11-08 21:05:04 -0600

    delete firestore

--- src/view/tablerow.js
+++ src/view/tablerow.js
@@ -13,9 +13,17 @@ class TableRows extends Component {
         this.handle_resize = this.handle_resize.bind(this);
     }
 
-    draw_tables(height) {
+    draw_tables(height, button_color) {
         var tab = [];
         var marg = height * .2
+        var btn_color;
+        if (button_color == 0) {
+            btn_color = 'btn-light'
+        } else {
+            btn_color = 'btn-dark'
+        }
+        var active = ''
+
         for (var i = 0; i < this.props.tables.length; i++) {
             var color = '';
             switch (this.props.tables[i]['status']) {
@@ -40,7 +48,10 @@ class TableRows extends Component {
                     break;
             }
             var square = (
-                <div style={ { width:height, height:height, marginLeft:marg, marginRight:marg, marginTop:marg, marginBottom:marg, 'backgroundColor':color } }></div>
+                <div class="d-flex align-item-center flex-column" >
+                    <div style={ { width:height, height:height, marginLeft:marg, marginRight:marg, marginTop:marg, 'backgroundColor':color } }></div>
+                    <button class={`btn ${btn_color}`} style={ { marginLeft: marg, marginRight: marg } } >reserve</button>
+                </div>
             );
 
             tab.push(square);
@@ -64,18 +75,21 @@ class TableRows extends Component {
     }
 
     render() {
-        var color = 0
+        var color
+        var color_button
         if (this.props.index % 2 === 0) {
             color = '#444444'
+            color_button = 0
         }
         else {
             color = '#ffffff'
+            color_button = 1
         }
 
         var height = this.state['height'] * .6;
         var t;
         if (this.state['mounted']) {
-            t = this.draw_tables(height);
+            t = this.draw_tables(height, color_button);
         }
 
         return (
