263943b337f9c2e0f35c166d8c66001935c590a6
unknown <auchiyam1994@gmail.com>
2018-11-08 20:26:08 -0600

    Updates

---
+++ precache-manifest.f7cefa79ab5befe61beab7d80d9811a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3fe1b326a6a9d37c172",
+    "url": "/StudySeatHost/static/js/runtime~main.a3fe1b32.js"
+  },
+  {
+    "revision": "e99ffee47b9f3683d926",
+    "url": "/StudySeatHost/static/js/main.e99ffee4.chunk.js"
+  },
+  {
+    "revision": "5ead528054704b17c510",
+    "url": "/StudySeatHost/static/js/1.5ead5280.chunk.js"
+  },
+  {
+    "revision": "e99ffee47b9f3683d926",
+    "url": "/StudySeatHost/static/css/main.39cce741.chunk.css"
+  },
+  {
+    "revision": "c4132b5e24be3544522bbaf69f4615a2",
+    "url": "/StudySeatHost/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/study-seat/precache-manifest.0a12e4f70f7a9b0024808eab60cf5dd4.js"
+  "/StudySeatHost/precache-manifest.f7cefa79ab5befe61beab7d80d9811a5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/study-seat/index.html", {
+workbox.routing.registerNavigationRoute("/StudySeatHost/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
